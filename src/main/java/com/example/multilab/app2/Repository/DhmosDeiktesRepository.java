8fa97fa8d19a5dd18a27aebd64c3b827b3e14cd5
nikosmel <meletiou.nikos1@gmail.com>
2018-04-13 21:54:11 +0300

    findDhmosDeiktesByArxh query fix

--- src/main/java/com/example/multilab/app2/Repository/DhmosDeiktesRepository.java
+++ src/main/java/com/example/multilab/app2/Repository/DhmosDeiktesRepository.java
@@ -16,7 +16,8 @@ public interface DhmosDeiktesRepository extends JpaRepository<DhmosDeiktes,Long>
     @Query(value = "SELECT * FROM deiktes d WHERE d.ID=?1",nativeQuery = true)
     DhmosDeiktes findDhmosDeiktesById(Long id);
 
-    @Query(value = "SELECT dd FROM dhmos_deiktes as dd , deiktes as dei ,aksones as aks " +
-            "WHERE dd.DEIKTES_ID = dei.ID AND dei.AKSONES_ID = aks.ID AND aks.ARXES_ID = ?1")
+    @Query(value = "SELECT dd.ID,dd.DHMOS_ID,dd.DEIKTES_ID,dd.STADIO FROM dhmos_deiktes as dd , deiktes as dei ,aksones as aks " +
+            "WHERE dd.DEIKTES_ID = dei.ID AND dei.AKSONES_ID = aks.ID AND aks.ARXES_ID = ?1",
+            nativeQuery = true)
     List<DhmosDeiktes> findDhmosDeiktesByArxh(Long id);
 }
